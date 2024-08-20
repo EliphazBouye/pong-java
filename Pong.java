3ccf2bd5f40117b283e780d0590d0aaf54a07490
Eliphaz Bouye <eliphazbouye@gmail.com>
2024-08-20 01:00:08 +0000

    feat: draw two players

--- Pong.java
+++ Pong.java
@@ -4,30 +4,41 @@ import java.awt.Color;
 import javax.swing.*;
 
 class Pong extends JPanel {
-    private static final int RECT_X  = 5;
-    private static final int RECT_Y  = 50;
+    private static final int DIMENSION_WIDTH = 900;
+    private static final int DIMENSION_HEIGHT = 600;
     private static final int RECT_WIDTH  = 20;
-    private static final int RECT_HEIGHT  = 160;
-    private Color shapeColor = Color.RED;
+    private static final int RECT_HEIGHT  = 150;
+    private static final int RECT_X  = 0;
+    private static final int RECT_Y  = DIMENSION_HEIGHT / 2 - RECT_HEIGHT / 2 ;
+    
+    private Color colorPlayer1 = Color.RED;
+    private Color colorPlayer2 = Color.GREEN;
 
     @Override
     protected void paintComponent(Graphics g) {
 	super.paintComponent(g);
-	g.setColor(shapeColor);
-	g.drawRect(RECT_X, RECT_Y, RECT_WIDTH, RECT_HEIGHT);
-	g.drawRect(80, RECT_Y, RECT_WIDTH, RECT_HEIGHT);
+
+	setBackground(Color.black);
+	player(g);
     }
 
     @Override
     public Dimension getPreferredSize() {
-	return new Dimension(900, 600);
+	return new Dimension(DIMENSION_WIDTH, DIMENSION_HEIGHT);
     }
+
+    private void player(Graphics g) {
+	g.setColor(colorPlayer1);
+	g.fillRect(RECT_X, RECT_Y, RECT_WIDTH, RECT_HEIGHT);
+	
+	g.setColor(colorPlayer2);
+	g.fillRect(DIMENSION_WIDTH - RECT_WIDTH, RECT_Y, RECT_WIDTH, RECT_HEIGHT);
+    } 
 
     private static void createAndShowGui() {
 	Pong mainPanel = new Pong();
 
 	JFrame frame = new JFrame("Pong");
-	frame.setBackground(Color.black);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	frame.getContentPane().add(mainPanel);
        	frame.pack();
